57a16df13983fdc8f49b6f49947101aa8faab5b8
admin123 <admin123@admin>
2021-12-22 19:17:42 +0530

    multiplication updates

---
+++ src/com/careerit/cj/demo/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package com.careerit.cj.demo;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Enter the value of n:");
+	int num=sc.nextInt();
+
+	
+	for(int i = 1;i<10;i++){
+		System.out.println(num+" * "+ i +" = "+num*i);
+		
+	}
+
+	
+	
+	
+}
+}
